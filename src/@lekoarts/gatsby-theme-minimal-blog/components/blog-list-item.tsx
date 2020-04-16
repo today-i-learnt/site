cce2e4f516186156d9997ffac9ac5bc35d44bfe3
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2020-04-17 01:53:11 +0530

    Format date in post metadata

--- src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
@@ -4,6 +4,7 @@ import { jsx, Styled } from "theme-ui"
 import { Box } from "@theme-ui/components"
 import { Link } from "gatsby"
 import ItemTags from "./item-tags"
+import { formatDateForTitle, formatDateForMeta } from "../utils/dates"
 
 type BlogListItemProps = {
   post: {
@@ -21,30 +22,24 @@ type BlogListItemProps = {
   showTags?: boolean
 }
 
-function formatDate(dateStr: string) {
-  if(!dateStr) {
-    return '';
-  }
 
-  return (new Date(dateStr)).toLocaleDateString(undefined, { month: 'short', day: 'numeric' });
-}
 
 const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
   <Box mb={4}>
     <Styled.a as={Link} to={post.slug} sx={{ fontSize: [1, 2, 3], color: `text` }}>
-      {formatDate(post.date)}: {post.title}
+      {formatDateForTitle(post.date)}: {post.title}
     </Styled.a>
-    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 2, 2] }}>
-      {post.excerpt}
-    </p>
-    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
-      <time>{post.date}</time>
+    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [0.75, 0.8, 1] }}>
+      <time dateTime={post.date}>{formatDateForMeta(post.date)}</time>
       {post.tags && showTags && (
         <React.Fragment>
           {` — `}
           <ItemTags tags={post.tags} />
         </React.Fragment>
       )}
+    </p>
+    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 2, 2] }}>
+      {post.excerpt}
     </p>
   </Box>
 )
---
+++ src/@lekoarts/gatsby-theme-minimal-blog/utils/dates.ts
@@ -0,0 +1,15 @@
+export function formatDateForTitle(dateStr: string) {
+  if (!dateStr) {
+    return '';
+  }
+
+  return (new Date(dateStr)).toLocaleDateString(undefined, { month: 'short', day: 'numeric' });
+}
+
+export function formatDateForMeta(dateStr: string) {
+  if (!dateStr) {
+    return '';
+  }
+
+  return (new Date(dateStr)).toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric' });
+}
