1d43c851a6aa0c29490bf1f6c93723917a83cbfb
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2020-06-11 04:36:37 +0530

    Customize 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import Layout from '../components/layout/layout'
 import SEO from '../components/seo/seo'
+import notFound from './img/not-found.svg'
+import './404.scss'
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout className="NotFound">
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>There's nothing here..</h1>
+    <img src={notFound} />
+    <p>You just hit a route that doesn&#39;t exist...</p>
   </Layout>
 )
 
